ea0dd012e07af1d2ab0f90a68d6b434e14defc90
Shankar Kantharaj <shankar.kantharaj@live.com>
2017-09-08 18:01:47 +0000

    Test

---
+++ SpecFlowTestSuite/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SpecFlowTestSuite")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SpecFlowTestSuite")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("36a71be7-3e57-428e-8b1d-da8a4b53fa4a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SpecFlowTestSuite/SampleTestCase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SpecFlowTestSuite
+{
+    [TestClass]
+    public class SampleTestCase
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
